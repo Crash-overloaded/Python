91079ecae7644677964f51dc17ea3f38070d98f3
Crash-overloaded <priyankagarwal00.pa@gmail.com>
2019-06-10 00:15:45 +0530

    changing path

--- problem2.py
+++ problem2.py
@@ -2,7 +2,7 @@
 
 from googlesearch import search
 import time
-raw_data=open('/root/Desktop/Python/search.txt','w')
+raw_data=open('search.txt','w')
 data=input("Enter something you want to search:-")
 for i in search(data,tld="co.in",stop=10):
 	print(i)
